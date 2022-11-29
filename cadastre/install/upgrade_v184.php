e9b917b44e5d0978afff6a066a6e11b847649e43
rldhont <rldhont@gmail.com>
2022-11-29 09:58:09 +0100

    Mise à jour 1.8.4 - réapplication des droits

---
+++ cadastre/install/upgrade_v184.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author    René-Luc Dhont
+ * @copyright 2022 3liz
+ *
+ * @see      http://3liz.com
+ *
+ * @license Mozilla Public License : http://www.mozilla.org/MPL/
+ */
+class cadastreModuleUpgrader_v184 extends jInstallerModule
+{
+    public $targetVersions = array('1.8.4');
+    public $date = '2022-11-29';
+
+    public function install()
+    {
+        if ($this->firstExec('acl2')) {
+            $this->useDbProfile('auth');
+
+            jAcl2DbManager::addSubject('cadastre.acces.donnees.proprio.simple', 'cadastre~search.cadastre.acces.donnees.proprio.simple', 'cadastre.subject.group');
+
+            // Add rights on group
+            if (jAcl2DbUserGroup::getGroup('cadastre_lizmap')) {
+                // reset rights
+                jAcl2DbManager::addRight(
+                    'cadastre_lizmap',
+                    'cadastre.use.search.tool'
+                );
+                jAcl2DbManager::addRight(
+                    'cadastre_lizmap',
+                    'cadastre.acces.donnees.proprio.simple'
+                );
+                jAcl2DbManager::addRight(
+                    'cadastre_lizmap',
+                    'cadastre.acces.donnees.proprio'
+                );
+            }
+        }
+    }
+}
