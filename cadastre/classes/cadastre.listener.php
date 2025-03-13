e0dcca669393e6cd6be8cba5a66f9ee33ce7a3e1
rldhont <rldhont@gmail.com>
2025-03-13 17:27:14 +0100

    Drop code related to QGIS 2
    Related to https://github.com/3liz/lizmap-cadastre-module/issues/94
    
    Superseeded https://github.com/3liz/lizmap-cadastre-module/pull/101

--- cadastre/classes/cadastre.listener.php
+++ cadastre/classes/cadastre.listener.php
@@ -5,59 +5,45 @@ class cadastreListener extends jEventListener
     public function ongetMapAdditions($event)
     {
         // Check cadastre configuration
-        $hasCadastreConfig = false;
-        $cadastreConfig = array();
-        $services = lizmap::getServices();
-        if (version_compare($services->qgisServerVersion, '3.0', '<')) {
-            if (preg_match('#^cadastre#i', $event->project)) {
-                $hasCadastreConfig = true;
-                $cadastreConfig = array(
-                    'layer' => 'Parcelles',
-                    'pk' => 'geo_parcelle',
-                );
-            }
-        } else {
-            $config = cadastreConfig::get($event->repository, $event->project);
-            if ($config !== null) {
-                $hasCadastreConfig = true;
-                $cadastreConfig = array(
-                    'layer' => $config->parcelle->name,
-                    'pk' => $config->parcelle->unique_field,
-                );
-                $cadastreConfig = array_merge(
-                    $cadastreConfig,
-                    (array) $config
-                );
-            }
+        $config = cadastreConfig::get($event->repository, $event->project);
+        if ($config === null) {
+            return;
         }
-
-        // Add JS and CSS for cadastre module
-        $js = array();
-        $jscode = array();
-        $css = array();
 
         // Add the cadastre config only if at least one right is granted
-        $hasEnoughRights = (
-            jAcl2::check('cadastre.use.search.tool')
-            || jAcl2::check('cadastre.acces.donnees.proprio')
-            || jAcl2::check('cadastre.acces.donnees.proprio.simple')
+        // Check if the user has enough rights
+        if (!(
+            jAcl2::check('cadastre.use.search.tool') ||
+            jAcl2::check('cadastre.acces.donnees.proprio') ||
+            jAcl2::check('cadastre.acces.donnees.proprio.simple')
+        )) {
+            return;
+        }
+
+        $cadastreConfig = array(
+            'layer' => $config->parcelle->name,
+            'pk' => $config->parcelle->unique_field,
+        );
+        $cadastreConfig = array_merge(
+            $cadastreConfig,
+            (array) $config
         );
 
-        if ($hasCadastreConfig && $hasEnoughRights) {
-            $js = array(
-                jUrl::get('jelix~www:getfile', array('targetmodule' => 'cadastre', 'file' => 'cadastre.js')),
-            );
+        // Add JS and CSS for cadastre module
+        $js = array(
+            jUrl::get('jelix~www:getfile', array('targetmodule' => 'cadastre', 'file' => 'cadastre.js')),
+        );
 
-            $cadastreConfig['url'] = jUrl::get('cadastre~service:initExport');
-            $cadastreConfig['advanced'] = jAcl2::check('cadastre.acces.donnees.proprio');
+        $cadastreConfig['url'] = jUrl::get('cadastre~service:initExport');
+        $cadastreConfig['advanced'] = jAcl2::check('cadastre.acces.donnees.proprio');
 
-            $jscode = array(
-                'var cadastreConfig = ' . json_encode($cadastreConfig),
-            );
-            $css = array(
-                jUrl::get('jelix~www:getfile', array('targetmodule' => 'cadastre', 'file' => 'cadastre.css')),
-            );
-        }
+        $jscode = array(
+            'var cadastreConfig = ' . json_encode($cadastreConfig),
+        );
+        $css = array(
+            jUrl::get('jelix~www:getfile', array('targetmodule' => 'cadastre', 'file' => 'cadastre.css')),
+        );
+
         $event->add(
             array(
                 'js' => $js,
--- cadastre/controllers/service.classic.php
+++ cadastre/controllers/service.classic.php
@@ -31,14 +31,6 @@ class serviceCtrl extends jController
             return $rep;
         }
 
-        $services = lizmap::getServices();
-        if (version_compare($services->qgisServerVersion, '3.0', '<')
-            && !preg_match('#^cadastre#i', $project)) {
-            $rep->data = array('status' => 'error', 'message' => 'This is not a cadastre project. Project key must begins with cadastre');
-
-            return $rep;
-        }
-
         $p = lizmap::getProject($repository . '~' . $project);
         if (!$p) {
             $rep->data = array('status' => 'error', 'message' => 'A problem occurred while loading project with Lizmap');
@@ -46,27 +38,29 @@ class serviceCtrl extends jController
             return $rep;
         }
 
-        jClasses::inc('cadastre~lizmapCadastreRequest');
-        if (version_compare($services->qgisServerVersion, '3.0', '>=')) {
-            $request = new lizmapCadastreRequest(
-                $p,
-                array(
-                    'service' => 'CADASTRE',
-                    'version' => '1.0.0',
-                    'request' => 'GetCapabilities',
-                )
-            );
-            $result = $request->process();
-            if ($result->code !== 200) {
-                $rep->data = array('status' => 'error', 'message' => 'Cadastre module is not well installed.');
-
-                return $rep;
-            }
-        }
-
         $config = cadastreConfig::get($repository, $project);
         if ($config === null) {
             $rep->data = array('status' => 'error', 'message' => 'This is not a cadastre project or has not been configured.');
+
+            return $rep;
+        }
+
+        jClasses::inc('cadastre~lizmapCadastreRequest');
+        $request = new lizmapCadastreRequest(
+            $p,
+            array(
+                'service' => 'CADASTRE',
+                'version' => '1.0.0',
+                'request' => 'GetCapabilities',
+            )
+        );
+        $result = $request->process();
+        if ($result->code !== 200) {
+            $rep->data = array(
+                'code' => $result->code,
+                'status' => 'error',
+                'message' => 'Cadastre module is not well installed.'
+            );
 
             return $rep;
         }
@@ -245,10 +239,7 @@ class serviceCtrl extends jController
         // The Python script, if available, if run asynchronously
         // and a new Export page is shown to show the user the progress
         // If not, use a synchronous call to QGIS Server, which could reach timeouts for big export (commune)
-        $standalone_python_script = '/srv/qgis/plugins/cadastre/standalone_export.py';
-        if (version_compare($services->qgisServerVersion, '3.0', '>=')) {
-            $standalone_python_script = '/srv/qgis/plugins/cadastre/standalone/export.py';
-        }
+        $standalone_python_script = '/srv/qgis/plugins/cadastre/standalone/export.py';
         // Do not use the Python script if we only need to get the HTML content
         // Which is retrieved in AJAX calls. Hence the or $type == fiche call
         if (!is_file($standalone_python_script) or $type != 'proprietaire') {
@@ -278,10 +269,7 @@ class serviceCtrl extends jController
         $log = jApp::tempPath($token . '.log');
 
         // Run python code
-        $cmd = 'python ' . $standalone_python_script;
-        if (version_compare($services->qgisServerVersion, '3.0', '>=')) {
-            $cmd = 'python3 ' . $standalone_python_script;
-        }
+        $cmd = 'python3 ' . $standalone_python_script;
         $cmd .= ' -P ' . $p->getQgisPath();
         $cmd .= ' -L "' . $parcelleLayer . '"';
         $cmd .= ' -I ' . $parcelleId;
